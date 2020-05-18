2209d38611f36512e0164b487193d862d60191c1
jacksonschuler <jacksonschuler11@gmail.com>
2020-05-18 10:30:38 -0600

    file structure for components on front-end

---
+++ src/components/initial-screen/Initial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '@material-ui/core/Card'
+
+function Initial() {
+    return (
+        <div>
+            Initial Screen
+        </div>
+    );
+}
+
+export default Initial;
