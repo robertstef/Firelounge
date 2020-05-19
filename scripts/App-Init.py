fbbd8566a85b00792cf08c6ae443cb5c4e963513
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-05-19 12:15:56 -0600

    Init script creates a new users project file using their username

--- scripts/App-Init.py
+++ scripts/App-Init.py
@@ -3,12 +3,11 @@
 import os
 import subprocess
 
-home = os.path.expanduser("~/.firelounge/Users")
 
-def get_user():
+def get_user_filename():
     """
-    Obtains the username for the current user logged into firebase.
-    :return: the users username (in form of email address)
+    Obtains the filename for the current user logged into firebase.
+    :return: the users username (in form of email_address.json)
     """
     # run login command to get username
     output = subprocess.run(["firebase", "login", "--interactive"], stdout=subprocess.PIPE, universal_newlines=True)
@@ -16,10 +15,20 @@ def get_user():
     # extract username from output
     output = ([i for i in output.stdout.split('\n') if i][-1])
     output = ([i for i in output.split(' ') if i][-1])
+    output = output.split("@")[0][4:]
+    output = output + ".json"
 
-    return output[4:-5]
-
+    return output
 
 
 if __name__ == '__main__':
-    uname = get_user()
+
+    home = os.path.expanduser("~/.firelounge/Users")
+    ufile = get_user_filename()
+    path = os.path.join(home, ufile)
+
+    # first time firelounge user
+    if not os.path.isfile(path):
+        with open(path, 'w') as fn:
+            pass
+
