741513370a003a5489b2cbc8d8b3459fc65bf106
jacksonschuler <jacksonschuler11@gmail.com>
2020-05-21 15:15:41 -0600

    code cleanup in the create project screen

--- src/components/create-screen/CreateScreenCard.js
+++ src/components/create-screen/CreateScreenCard.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react'
 import Card from "@material-ui/core/Card";
-import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
+import {createMuiTheme} from "@material-ui/core/styles";
+import CreateNewProjectContent from "./CreateNewProjectContent";
+import CreateCurrentProjectContent from "./CreateCurrentProjectContent";
 
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ef223c'
+        },
+    }
+});
 
 class CreateScreenCard extends Component{
-
-
-    constructor(props) {
-        super(props);
-        this.state={
-            projectName: '',
-        }
-    }
-
     render(){
         return(
             <div style={{padding: 15, height:'100%',}}>
@@ -24,43 +24,13 @@ class CreateScreenCard extends Component{
                         <Card style={{height: '87vh',paddingLeft: 30,paddingRight: 20, paddingBottom: 20,paddingTop:20}}>
                             <Grid container spacing={3}>
                                 <Grid item style={{width:'48%'}}>
-                                    <div>
-                                        <Typography variant="h6" gutterBottom>Create a New Project</Typography>
-                                        <div style={{marginTop:15}}/>
-                                        <div>
-                                            <TextField
-                                                style={{width:'80%'}}
-                                                id="outlined-size-small"
-                                                size={'small'}
-                                                defaultValue="Enter your project name"
-                                                variant={"outlined"}
-                                            />
-                                        </div>
-                                        <div style={{marginTop:20}}/>
-                                        <Typography variant="h6" gutterBottom>Select Project's Features</Typography>
-                                        <div style={{marginTop:20}}/>
-                                    </div>
+                                    <CreateNewProjectContent/>
                                 </Grid>
                                 <Grid item>
                                     <Divider orientation={'vertical'} style={{height: '87vh'}}/>
                                 </Grid>
                                 <Grid item style={{width:'48%'}}>
-                                    <div>
-                                        <Typography variant="h6" gutterBottom>Initialize a Current Project</Typography>
-                                        <div style={{marginTop:15}}/>
-                                        <div>
-                                            <TextField
-                                                style={{width:'80%'}}
-                                                id="outlined-size-small"
-                                                size={'small'}
-                                                defaultValue="Enter your project name"
-                                                variant={"outlined"}
-                                            />
-                                        </div>
-                                        <div style={{marginTop:20}}/>
-                                        <Typography variant="h6" gutterBottom>Select Project's Features</Typography>
-                                        <div style={{marginTop:20}}/>
-                                    </div>
+                                    <CreateCurrentProjectContent/>
                                 </Grid>
                             </Grid>
                         </Card>
