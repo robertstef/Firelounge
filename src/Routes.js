80da8ac5fe931100e59d587480d04dd02d729b15
BenBaker <bakerbenjamyn@gmail.com>
2020-05-27 15:49:07 -0600

    reformatted to allow for react strict mode to be used

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,15 @@ import {HashRouter, Switch, Route} from 'react-router-dom';
 import Main from "./components/main-screen/Main";
 import Initial from "./components/initial-screen/Initial";
 
-const Routes =  (
-	<HashRouter>
-	 	<Switch>
-	    	<Route path="/project" component={Main} />
-	      	<Route exact path="/" component={Initial}/>
-	  	</Switch>
-	</HashRouter>
-)
+function Routes() {
+	return (
+		<HashRouter>
+		 	<Switch>
+		    	<Route path="/project" component={Main} />
+		      	<Route exact path="/" component={Initial}/>
+		  	</Switch>
+		</HashRouter>
+	)
+}
 
 export default Routes;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  	Routes,
+  	 <React.StrictMode>
+  		<Routes/>
+  	 </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
