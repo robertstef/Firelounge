d92724559f18d1bc086c74e178b39b00c404bb15
BenBaker <bakerbenjamyn@gmail.com>
2020-05-27 16:12:24 -0600

    added test case using spawn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const cors = require('cors')({ origin: true });
 const path = require('path')
 const bodyParser = require('body-parser')
 const isDev = require("electron-is-dev");
+const { spawn } = require('child_process');
 
 const app = express()
 
@@ -12,7 +13,21 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.get("/test", (req, res) => {
-	res.status(200).send('test');
+	    const test_spawn = spawn("pwd");
+
+        test_spawn.stdout.on('data', (data) => {
+            console.log(`stdout:\n ${data}`);
+            res.status(200).send(data);
+        });
+
+        test_spawn.stderr.on('data', (data) => {
+            console.error(`stderr:\n ${data}`);
+        });
+
+        test_spawn.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });	
+
 		return null;
 	})
 
