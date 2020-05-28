355026175392ca437a27f875e1799985867fe965
BenBaker <bakerbenjamyn@gmail.com>
2020-05-28 09:31:59 -0600

    added logging to express server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ const cors = require('cors')({ origin: true });
 const path = require('path')
 const bodyParser = require('body-parser')
 const isDev = require("electron-is-dev");
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
+
+
+//setup logging information
+const log = require('electron-log')
+log.transports.file.level = 'info';
+log.transports.file.file = 'electron-log.log';
+log.info('log message');
 
 const app = express()
 
@@ -13,23 +20,26 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.get("/test", (req, res) => {
-	    const test_spawn = spawn("pwd");
+	    const test_spawn = exec("node ./src/scripts/test.js");
 
         test_spawn.stdout.on('data', (data) => {
             console.log(`stdout:\n ${data}`);
+            log.info(data);
             res.status(200).send(data);
         });
 
         test_spawn.stderr.on('data', (data) => {
             console.error(`stderr:\n ${data}`);
+            log.info(data);
         });
 
         test_spawn.on('close', (code) => {
             console.log(`child process exited with code ${code}`);
+            log.info(code);
         });	
 
 		return null;
 	})
 
 
-app.listen(5000, () => console.log('App listening on port 5000'));
+app.listen(5000, () => log.info('App listening on port 5000'));
