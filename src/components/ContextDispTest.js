bce0f3b4c0f6be1f1f2a7e36871b7faa295213fd
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-06-01 17:52:54 -0600

    Changed context examples to reflect changes to User class

--- src/components/ContextDispTest.js
+++ src/components/ContextDispTest.js
@@ -7,13 +7,13 @@ export default function DispTest() {
     return (
         <div>
 
-            { /* EG 1: Display current active proj and change active proj */}
+            { /* EG 1: Display current active proj and change active proj
 
             <button onClick={() => dispatch({type: 'setActive',
-                                            args:{name: 'test_proj', path: "~/src/files", id: "321"}})}>
+                                            args:"321"})}>
                 Set Active
             </button>
-
+            */}
 
             {/* EG 2: List projects and add project
 
@@ -23,12 +23,12 @@ export default function DispTest() {
             </button>
             */}
 
-            {/* EG 3: Remove project
+            {/* EG 3: Remove project*/}
             <button onClick={() => dispatch({type: 'removeProj',
-                args:{name:"proj1", path: "./Users/proj1", id:"123"}})}>
+                args:"123"})}>
                 Remove Project
             </button>
-            */}
+
 
         </div>
     )
