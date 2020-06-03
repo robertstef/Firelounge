bc06e6e9daa296c554c8b55b65c47c621241063e
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-06-03 17:42:29 -0600

    Removed user prop from Main and added context provider - now all components have access to the User context

--- src/components/main-screen/Main.js
+++ src/components/main-screen/Main.js
@@ -1,28 +1,15 @@
 import React, {Component} from 'react';
 import MainNav from "../MainNav";
-import queryString from 'query-string';
+import {UserProvider} from "../../context/userContext";
 
 class Main extends Component{
-    state = {
-    	username: ''
-    }
-
-	componentDidMount(){
-		const values = queryString.parse(this.props.location.search);
-		
-        //get user name from params and set state - so it rerenders
-        //lower components with username
-		this.setState({
-			username: values.username
-		})
-		
-	}
-
 
     render() {
         return(
             <div>
-                <MainNav username={this.state.username}/>
+                <UserProvider>
+                    <MainNav />
+                </UserProvider>
             </div>
         )
     }
