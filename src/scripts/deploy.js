c516f3e628cd908cab4dcb62c9d4c0cda74e0139
jacksonschuler <jacksonschuler11@gmail.com>
2020-06-03 14:27:11 -0600

    deploy script is now called in the active project directory

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -4,40 +4,44 @@
  * @param: options - json object {'hosting': true, 'storage': false, ...}
  */
 
-const {spawn} = require('child_process');
 
 function deploy(options) {
+
+    //var user_json = require('../Users/' + username + '.json');
+
+    var user_json = require("../Users/testusername.json");
+
+    var active_proj = user_json.activeProject; // get the active project of the current user
+
+    if (active_proj === undefined) {
+        console.log("NO ACTIVE PROJECT");
+        return;
+    }
+
+    var active_path = user_json.projects[active_proj].path.toString(); // get the path to the active project
+
+    const {exec} = require('child_process');
+
+    // execute the corresponding firebase deploy command based on options selected in the users current active project directory.
     if (options.all === true) {
-        const fb_deploy = spawn('firebase', ['deploy']);
-
-        fb_deploy.stdout.on('data', (data) => {
-            console.log(`stdout:\n ${data}`);
-        });
-
-        fb_deploy.stderr.on('data', (data) => {
-            console.error(`stderr:\n ${data}`);
-        });
-
-        fb_deploy.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
+        exec("firebase deploy", {cwd: active_path}, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`DEPLOY UNSUCCESSFUL:     ERROR: ${error}`);
+                return;
+            }
+            console.log("DEPLOY SUCCESS");
         });
 
     } else {
         let options_arr = Object.keys(options)
             .filter(function(k){return options[k]})
             .map(String);
-        const fb_deploy_options = spawn('firebase', ['deploy', '--only', options_arr.join(',')]);
-        fb_deploy_options.stdout.on('data', (data) => {
-            console.log(`stdout:\n ${data}`);
+        exec("firebase deploy --only" + options_arr.join(','), {cwd: active_path}, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`DEPLOY UNSUCCESSFUL:     ERROR: ${error}`);
+                return;
+            }
+            console.log("DEPLOY SUCCESS");
         });
-        fb_deploy_options.stderr.on('data', (data) => {
-            console.error(`stderr:\n ${data}`);
-        });
-        fb_deploy_options.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
-
     }
 }
-
-
