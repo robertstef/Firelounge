9323987cc20b7a28aa011742e455de621802ac02
BenBaker <bakerbenjamyn@gmail.com>
2020-06-12 13:13:22 -0600

    added ability to add new elements to database

--- src/components/manage-screen/db-screen/DbObjectDisplay.js
+++ src/components/manage-screen/db-screen/DbObjectDisplay.js
@@ -1,6 +1,36 @@
 import React, { Component} from 'react'
 import ReactJson from 'react-json-view';
 
+
+/*  
+    Used to compare two different Json Objects 
+    Returns the different keys between them
+*/
+function diff(obj1, obj2) {
+    const result = {};
+    if (Object.is(obj1, obj2)) {
+        return undefined;
+    }
+    if (!obj2 || typeof obj2 !== 'object') {
+        return obj2;
+    }
+    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+            result[key] = obj2[key];
+        }
+        if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+            const value = diff(obj1[key], obj2[key]);
+            if (value !== undefined) {
+                result[key] = value;
+            }
+        }
+    });
+    return result;
+}
+
+
+/* ***** INITIALIZE FIREBASE ADMIN APP WILL BE MOVED WHEN DYNAMIC DB SELECTION IS POSSIBLE ****** */
+
 var admin = window.require("firebase-admin");
 
 // Fetch the service account key JSON file contents
@@ -15,12 +45,17 @@ admin.initializeApp({
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 var db = admin.database();
 var ref = db.ref();
+
+
+
 export default class DbObjectDisplay extends Component {
     state={
+        //object that displayed
         src: {}
     }
     
     componentDidMount() {
+        //handles display update of any changes made to database via firebase console or in app
         ref.on("value", (snapshot) => {
             this.setState({src: snapshot.val()})
         });
@@ -28,12 +63,9 @@ export default class DbObjectDisplay extends Component {
 
     /* 
         When object is edited...
-        Updates the display in Firelounge and updates the reference in Firebase
+        Updates the reference in Firebase
     */
     makeEdit(result){
-        //update object display in firelounge
-        this.setState({ src: result.updated_src })
-
         /*
             create string to traverse json object and update in firebase
             namespace = array of keys to get to changed value
@@ -42,11 +74,11 @@ export default class DbObjectDisplay extends Component {
         */
         let query_string = '';
         for(var i = 0; i < result.namespace.length; i++) {
-            console.log(result.namespace[i])
             query_string += result.namespace[i]
             query_string += '/'
         }
         
+        console.log(query_string)
         //traverse and update
         db.ref(query_string).update({
             [result.name] : result.new_value
@@ -54,6 +86,40 @@ export default class DbObjectDisplay extends Component {
 
     }
 
+    /* 
+        When object is added...
+        Updates the reference in Firebase with the new key and the string 'Null'
+        Actual null entries arent allowed in firebase 
+        Need to find a solution to adding nested components
+    */
+    makeAdd(result){
+        /*
+        Create string to traverse json object and update in firebase
+        namespace = array of keys to get to added value
+        name = parent key of item that is added
+        new_value = new key being added
+        */
+
+        let query_string = '';
+        for(var i = 0; i < result.namespace.length; i++) {
+            query_string += result.namespace[i]
+            query_string += '/'
+        }
+
+        query_string += result.name
+        query_string += '/'
+
+        //get difference between old and new objects -- seemed like bad practice to update entire object
+        var keys = diff(result.new_value, result.existing_value)
+        var newKey = Object.keys(keys)[0]
+
+        //traverse and update
+        db.ref(query_string).update({
+            [newKey] : 'NULL'
+        })
+
+
+    }
 
     render() {
         var onEdit = true
@@ -75,8 +141,7 @@ export default class DbObjectDisplay extends Component {
                     onAdd={
                         onAdd
                             ? result => {
-                                  console.log(result)
-                                  this.setState({ src: result.updated_src })
+                                  this.makeAdd(result)
                               }
                             : false
                     }
