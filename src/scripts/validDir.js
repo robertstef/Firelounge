530fb4138ceb0f4b1e67512853e08bb592dfaeac
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-06-13 16:49:18 -0600

    Added check for firebase.json. Refactored to check for file existence with existsSync() instead of access()

--- src/scripts/validDir.js
+++ src/scripts/validDir.js
@@ -8,16 +8,18 @@ module.exports = {
             }
 
             //check for .firebaserc file -- can add other files her when needed
-            const path_rc = path.concat('/.firebaserc')
+            const path_rc = path.concat('/.firebaserc');
+            const path_json = path.concat('/firebase.json');
 
-            //attempt access
-            fs.access(path_rc, fs.F_OK, (err) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(1)
-                }
-            });
+            // .firebaserc
+            if ( ! fs.existsSync(path_rc) ) {
+                reject("Invalid directory, does not contain .firebaserc file");
+            }
+            // firebase.json
+            else if ( ! fs.existsSync(path_json) ) {
+                reject("Invalid directory, does not contain firebase.json file");
+            }
+            else { resolve(1) }
         });
     }
 };
