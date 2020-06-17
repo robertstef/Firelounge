d7c1f998d7e0bc0d3feef4118ab144c530fa04b5
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-06-16 21:08:43 -0600

    Wrote function to create functions directory in users project directory

--- src/scripts/createProject/functionsInitHelper.js
+++ src/scripts/createProject/functionsInitHelper.js
@@ -39,16 +39,58 @@ let check_input = (input) => {
     }
 }
 
+/**
+ * Produces a random 6 digit hexidecimal number.
+ * @returns {string}: 6 digit hex number
+ */
+let random_hex = () => {
+    var letters = '0123456789abcdef';
+    var hex_val = '';
+    for (var i = 0; i < 6; i++) {
+        hex_val += letters[Math.floor(Math.random() * 16)];
+    }
+    return hex_val;
+}
+
+/**
+ * Creates the functions directory in the users project directory
+ * specified by the inputted path.
+ * @param dir_path: String - path to users project directory
+ */
 let write_fcns_dir = (dir_path) => {
     let fcns_dir = path.join(dir_path, "/functions");
 
-    // check if user has a directory named functions in their
-    // folder already
+    // User already has a directory named functions
     if (fs.existsSync(fcns_dir)) {
-
+        // try naming dir functions-fb
+        let fb_path = fcns_dir + "-fb";
+        if (! fs.existsSync(fb_path)) {
+            fs.mkdirSync(fb_path);
+            fcns_dir = fb_path;
+        }
+        // for some reason that also exists name using a random hex value
+        else {
+            let flag = true;
+            // keep producing random hex values until we find one that works --
+            // maybe overkill but will ensure we don't overwrite any of their data
+            while (flag) {
+                let hex_val = random_hex();
+                let hex_path = fcns_dir + "-" + hex_val;
+                if (! fs.existsSync(hex_path)) {
+                    fs.mkdirSync(hex_path);
+                    fcns_dir = hex_path;
+                    flag = false;
+                }
+            }
+        }
     }
+    // no pre-existing functions file
+    else {
+        fs.mkdirSync(fcns_dir);
+    }
+    // TODO set RESOURCE_DIR env variable for use by firebase.json
 }
 
-write_fcns_dir();
 /* Export statements */
 exports.check_input = check_input;
+exports.write_fcns_dir = write_fcns_dir;
