ac67349568e1ff0d2deb75d8098909076f37d926
BenBaker <bakerbenjamyn@gmail.com>
2020-07-01 11:40:15 -0600

    add circular progress wheel and logic when app starts while user object is loaded

---
+++ src/components/main-screen/CircularProgress.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      margin: theme.spacing(20),
+    },
+    'backgroundColor': 'grey',
+    'opacity': 0.5,
+    'position': 'absolute',
+    'zIndex': 2000,
+    'top': 0,
+    'left': 0,
+    'bottom': 0,
+    'right': 0,
+  },
+}));
+
+export default function CircularIndeterminate(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  );
+}
--- src/components/main-screen/Main.js
+++ src/components/main-screen/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import MainNav from "../MainNav";
 import LoginAlert from "./LoginAlert";
+import CircularProgress from "./CircularProgress"
 import {UserDispatch, UserState} from "../../context/userContext";
 const ui = require('../../scripts/userInfo');
 const initModule = require('../../scripts/init');
@@ -9,23 +10,36 @@ function Main() {
     const dispatch = UserDispatch();
     const {user} = UserState();
     const [showModal, setShowModal] = useState(true)
+    const [loading, setLoading] = useState(true)
 
     useEffect( () => {
         async function fetchUsername() {
             let user_check = await ui.user_info();
-
+            
             if(user_check.name !== undefined || user_check.name !== '' ) {
                 initModule.init_function().then(async (output) => {
                     await dispatch({type: 'createUser', args: output});
+                    setLoading(false)
+                    setShowModal(false)
                 }).catch(err => {
                     console.log(err);
                 })
-                setShowModal(false)
+            } else {
+                setLoading(false)
             }
         }
         fetchUsername();
     }, [])
-    
+
+    if(loading) {
+        return(
+            <div>
+            <MainNav />
+            <CircularProgress />
+        </div>
+        )
+    }
+
     return(
         <div>
             <MainNav />
