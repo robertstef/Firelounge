ced9e991889daf17dd3f7dbb15c305f7ad432c8c
BenBaker <bakerbenjamyn@gmail.com>
2020-07-02 14:04:05 -0600

    fixed bug where tables wasnt displaying correct projects

--- src/components/create-screen/Table.js
+++ src/components/create-screen/Table.js
@@ -70,8 +70,8 @@ export default function CustomizedTable() {
 
     const classes = useStyles();
     const {user} = UserState();
-    let projects = user.fb_projs;
-
+    let projects = user.firebase_projs;
+    
     /* Create rows of firebase projects for table */
     let rows = [];
     for (let p of projects) {
