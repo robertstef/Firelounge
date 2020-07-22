885712733ff3bdd451acd009f6e52ecab91cafc8
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-07-22 12:30:46 -0600

    Added methods to remove parenthesis and replace chars matching regex

---
+++ src/queries-Robert/parser/queryHelper.js
@@ -0,0 +1,37 @@
+module.exports = {
+    regexIndexOf: (string, regex, startpos) => {
+        let substr = string.substring(startpos || 0);
+        let indexOf = substr.search(regex);
+
+        if (indexOf >= 0) {
+            return indexOf + (startpos || 0);
+        } else {
+            return indexOf;
+        }
+    },
+
+    /**
+     * Replaces the substring matching the given
+     * given regex within string with replacement.
+     * @param string: String
+     * @param regex: Regex
+     * @param replacement: String
+     * @returns {String}: the modified string
+     */
+    replaceAll: (string, regex, replacement) => {
+        return string.replace(new RegExp(regex, "g"), replacement);
+    },
+
+    /**
+     * Removes parenthesis surrounding the query.
+     * @param query: String
+     * @returns {string|*}: query with parenthesis removed
+     */
+    removeWrappedParenthesis: (query) => {
+        if (/^\(.+\)$/.test(query)) {
+            return query.substring(1, query.length - 1);
+        } else {
+            return query;
+        }
+    }
+}
