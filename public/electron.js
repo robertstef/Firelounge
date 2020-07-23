fe3f7fb6053f02fa85e34a97ee05aebc0dcc5d89
jacksonschuler <jacksonschuler11@gmail.com>
2020-07-23 12:23:00 -0600

    added icon png

--- public/electron.js
+++ public/electron.js
@@ -74,6 +74,7 @@ let win;
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({
+        icon: 'src/logo/FireLoungeLogo.svg',
         width: 1000,
         height: 600,
         webPreferences: {
