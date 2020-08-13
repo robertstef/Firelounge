cd6dee1a01825ff260e7a6e1d98f7d076df7fff9
BenBaker <bakerbenjamyn@gmail.com>
2020-08-13 13:12:09 -0600

    set up SettingsSwitchesGroup to read/write from/to User context

--- src/components/settings-screen/SettingsSwitchesGroup.js
+++ src/components/settings-screen/SettingsSwitchesGroup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -7,6 +7,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {UserDispatch, UserState} from "../../context/userContext";
+
 
 const useStyles = makeStyles((theme) => ({
     formGroup: {
@@ -23,90 +26,108 @@ const useStyles = makeStyles((theme) => ({
     },
     label: {
       fontSize: 12, 
-    }
+    },
+    button: {
+      position: 'absolute',
+      right: '10%',
+      bottom: '10%',
+      backgroundColor: '#8D99AE'
+  },
 }));
 
 /* 
 Switches Group controlls the settings for the
 JSON Object viewer 
 */
-//TODO: Still needs to be connect to a Context/Database Object
+
 //TODO: Still needs a few text fields to get non boolean setting fields
 //TODO: Change List to loop 
 export default function SettingsSwitchesGroup() {
   const classes = useStyles();
+  const dispatch = UserDispatch();
+  const {user} = UserState();  
   const [state, setState] = React.useState({
-    Edit: true,
     Add: false,
-    Delete: true,
-    Collapsed: true,
+    Edit: false,
+    Delete: false,
+    Collapsed: false,
     Clipboard: false,
     DisplayObjectSize: false,
     SortKeys: false,
-    ValidationMessage: false
-    //group Arrays
-    //theme
-    //inconStyle
+    DisplayDataType: false
   });
+
+
+  useEffect(() => {
+    setState(user.act_db_settings);
+  }, [])
+
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
+  const handleClick = async () => {
+    dispatch({type:"saveDbSettings", args: state});
+  }
+
   return (
-    <FormControl component="fieldset" className={classes.formGroup}>
-      <FormGroup>
-        <List dense={true} className={classes.list}>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.Edit} onChange={handleChange} name="Edit" />}
-               label={<Typography className={classes.label} >Edit</Typography>}
-            /> 
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.Add} onChange={handleChange} name="Add" />}
-              label={<Typography className={classes.label} >Add</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.Delete} onChange={handleChange} name="Delete" />}
-              label={<Typography className={classes.label} >Delete</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.Collapsed} onChange={handleChange} name="Collapsed" />}
-              label={<Typography className={classes.label} >Collapsed</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.Clipboard} onChange={handleChange} name="Clipboard" />}
-              label={<Typography className={classes.label} >Clipboard</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.DisplayObjectSize} onChange={handleChange} name="DisplayObjectSize" />}
-              label={<Typography className={classes.label} >DisplayObjectSize</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.SortKeys} onChange={handleChange} name="SortKeys" />}
-              label={<Typography className={classes.label} >SortKeys</Typography>}
-            />
-          </ListItem>
-          <ListItem className={classes.listItem} >
-            <FormControlLabel
-              control={<Switch checked={state.ValidationMessage} onChange={handleChange} name="ValidationMessage" />}
-              label={<Typography className={classes.label} >ValidationMessage</Typography>}
-            />
-          </ListItem>
-        </ List>
-      </FormGroup>
-    </FormControl>
+    <div>
+      <FormControl component="fieldset" className={classes.formGroup}>
+        <FormGroup>
+          <List dense={true} className={classes.list}>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.Edit} onChange={handleChange} name="Edit" />}
+                label={<Typography className={classes.label} >Edit</Typography>}
+              /> 
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.Add} onChange={handleChange} name="Add" />}
+                label={<Typography className={classes.label} >Add</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.Delete} onChange={handleChange} name="Delete" />}
+                label={<Typography className={classes.label} >Delete</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.Collapsed} onChange={handleChange} name="Collapsed" />}
+                label={<Typography className={classes.label} >Collapsed</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.Clipboard} onChange={handleChange} name="Clipboard" />}
+                label={<Typography className={classes.label} >Clipboard</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.DisplayObjectSize} onChange={handleChange} name="DisplayObjectSize" />}
+                label={<Typography className={classes.label} >DisplayObjectSize</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.SortKeys} onChange={handleChange} name="SortKeys" />}
+                label={<Typography className={classes.label} >SortKeys</Typography>}
+              />
+            </ListItem>
+            <ListItem className={classes.listItem} >
+              <FormControlLabel
+                control={<Switch checked={state.DisplayDataType} onChange={handleChange} name="DisplayDataType" />}
+                label={<Typography className={classes.label} >DisplayDataType</Typography>}
+              />
+            </ListItem>
+          </ List>
+        </FormGroup>
+      </FormControl>
+      <Button className={classes.button} color={'default'} onClick={handleClick}> Save </Button>
+    </div>
   );
 }
