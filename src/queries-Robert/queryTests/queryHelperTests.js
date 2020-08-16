5b4a1739f5fa9406a654d73c70215bfe43b357ad
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-16 16:34:20 -0600

    Wrote tests for determinComparatorAndIndex

---
+++ src/queries-Robert/queryTests/queryHelperTests.js
@@ -0,0 +1,43 @@
+const qh = require('../parser/queryHelper');
+const assert = require('assert');
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
+
+/* Tests for determineComparatorAndIndex */
+describe("determineComparatorAndIndex", () => {
+    it("Not equal with !=", () => {
+        let where = "player != game";
+        let {comparator, index} = qh.determineComparatorAndIndex(where);
+        assert.equal(comparator, "!=");
+        assert.equal(index, where.indexOf("!="));
+    });
+
+    it("Not equal with <>", () => {
+        let where = "player<>game";
+        let {comparator, index} = qh.determineComparatorAndIndex(where);
+        assert.equal(comparator, "!=");
+        assert.equal(index, where.indexOf("<>"));
+    });
+
+    it("Equals operator", () => {
+        let where = "this=that";
+        let {comparator, index} = qh.determineComparatorAndIndex(where);
+        assert.equal(comparator, "==");
+        assert.equal(index, where.indexOf("="));
+    });
+
+    it("Greater than operator", () => {
+        let where = "this > that";
+        let {comparator, index} = qh.determineComparatorAndIndex(where);
+        assert.equal(comparator, ">");
+        assert.equal(index, where.indexOf(">"));
+    });
+
+    it("Invalid operator", () => {
+        let where = "this !! that";
+        assert.throws(() => qh.determineComparatorAndIndex(where),
+            Error,
+            "determineComparatorAndIndex: invalid comparison operator");
+    });
+})
