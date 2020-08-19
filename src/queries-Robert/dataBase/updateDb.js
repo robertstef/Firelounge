31249d3dd47ff30ecdc7bf7794bd8029db059875
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-18 21:08:09 -0600

    Added function stub for UPDATE

---
+++ src/queries-Robert/dataBase/updateDb.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param path
+ * @param object
+ * @param fields
+ */
+let updateFields = (path, object, fields) => {
+    // TODO - UPDATE
+}
---
+++ src/queries-Robert/queryRunners/update.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param query
+ * @param user
+ */
+let executeUpdate = (query, user) => {
+    // TODO - UPDATE
+}
+
+/**
+ *
+ * @param data
+ * @param sets
+ */
+let generatePayload = (data, sets) => {
+   // TODO - UPDATE
+}
+
+/**
+ *
+ * @param obj
+ * @param sets
+ */
+let updateItemWithSets = (obj, sets) => {
+    // TODO - UPDATE
+}
