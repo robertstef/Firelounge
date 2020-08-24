252db7bdf85021782a4eb5759a50b51d8af34967
BenBaker <bakerbenjamyn@gmail.com>
2020-08-24 15:11:59 -0600

    refactored testing files

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const kill = require("tree-kill");
 const puppeteer = require("puppeteer-core");
 const { spawn } = require("child_process");
 
+
 const port = 9200; // Debugging port
 const timeout = 35000; // Timeout in miliseconds
 let page;
@@ -40,6 +41,7 @@ beforeAll(async () => {
     }
   }
 });
+
 /* function that runs after each test suite */
 afterAll(async () => {
   try {
@@ -49,69 +51,32 @@ afterAll(async () => {
   }
 });
 
-describe("Initialization Tests", () => {
+describe('Initialization Tests...', () => {
+  const {test1} = require("./helpers/InitalizationTests.js");
 
-  /* Checks that the initial page contains the correct header */
-  test("Content of Landing Page", async () => {
-    
-    await page.waitFor(20000);
-
-    let text;
-    await page.waitForSelector("#create-header");
-    text = await page.$eval("#create-header", element => element.innerText);
-    expect(text).toBe("Create a new FireLounge project");
-
-  });    
-  
-  //add more test to make sure all of the elements are there
-});
-
-describe("Adding Existing Project", () => {
-  /* Adds a existing project to firebase */
-  test("Content of Create Existing Project", async () => {
-    let text;
-    
-    //wait for dev server to start, init script to run
-    await page.waitFor(20000);
-    
-    //navigate to add existing project page
-    await page.waitForSelector("#create-existing-proj-icon");
-    await page.click("#create-existing-proj-icon");
-    
-    //confirm what page were on
-    await page.waitForSelector("#create-existing-proj-header" );
-    text = await page.$eval("#create-existing-proj-header", element => element.innerText);
-    expect(text).toBe("Already Have a Firebase Project?");
-    
+  test('Correct Landing Page Title', async () => {
+    await test1(page);
   });
-
-  test("Create Existing Project Execution", async () => {
-    // click to add project 
-    await page.waitForSelector("#create-existing-proj-button");
-    await page.$eval( "#create-existing-proj-button", form => form.click() );
-
-    // click to add project 
-    await page.waitForSelector("#create-existing-proj-alert");
-    text = await page.$eval("#create-existing-proj-alert", element => element.innerText);
-    expect(text).toBe("Project Successfully Imported to Firelounge");
-  });
-
-  test("Create Existing Project Navigate to Manage Page", async () => {
-    // click to navigate to deploy page 
-    await page.waitForSelector("#manage-proj-icon");
-    await page.click("#manage-proj-icon");
-
-    // confirm proj id matches recently added project
-    await page.waitForSelector("#manage-deploy-proj-title");
-    text = await page.$eval("#manage-deploy-proj-title", element => element.innerText);
-    expect(text).toBe("bensnewproject");
-  });
-
-  // test("Create Existing Project Confirmation", async () => {
-
-  // });
 
 });
 
+
+describe('Create Existing Project', () => {
+  const {test1, test2, test3, test4} = require("./helpers/CreateExistingProject.js");
+
+  test('Content of Create Existing Project', async () => {
+    await test1(page);
+  });
+
+  test('Create Existing Project Execution', async () => {
+    await test2(page);
+  });
+
+  test('Create Existing Project Navigate to Manage Page', async () => {
+    await test3(page);
+  });
   
- 
+  test('Create Existing Project Confirmation', async () => {
+    await test4(page);
+  });
+});
---
+++ test/helpers/CreateExistingProject.js
@@ -0,0 +1,50 @@
+/* Adds a existing project to firebase */
+module.exports = {
+    
+    /* Content of Create Existing Project */
+    test1: async function (page) {
+        let text;
+    
+        //wait for dev server to start, init script to run
+        await page.waitFor(20000);
+        
+        //navigate to add existing project page
+        await page.waitForSelector("#create-existing-proj-icon");
+        await page.click("#create-existing-proj-icon");
+        
+        //confirm what page were on
+        await page.waitForSelector("#create-existing-proj-header" );
+        text = await page.$eval("#create-existing-proj-header", element => element.innerText);
+        expect(text).toBe("Already Have a Firebase Project?");
+    },
+
+    /* Create Existing Project Execution */
+    test2: async function (page) {
+        // click to add project 
+        await page.waitForSelector("#create-existing-proj-button");
+        await page.$eval( "#create-existing-proj-button", form => form.click() );
+
+        // click to add project 
+        await page.waitForSelector("#create-existing-proj-alert");
+        text = await page.$eval("#create-existing-proj-alert", element => element.innerText);
+        expect(text).toBe("Project Successfully Imported to Firelounge");  
+    },
+    
+    /* Create Existing Project Navigate to Manage Page */ 
+    test3: async function (page) {
+        // click to navigate to deploy page 
+        await page.waitForSelector("#manage-proj-icon");
+        await page.click("#manage-proj-icon");
+
+        // confirm proj id matches recently added project
+        await page.waitForSelector("#manage-deploy-proj-title");
+        text = await page.$eval("#manage-deploy-proj-title", element => element.innerText);
+        expect(text).toBe("bensnewproject");
+    },
+    
+    /* Create Existing Project Confirmation */ 
+    test4: async function (page) {
+        //TODO: Check active project title
+    },
+    
+}
