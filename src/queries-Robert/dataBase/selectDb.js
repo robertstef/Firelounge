8fbeeedba45cc2fe74cfa8b92913d7470425b7c9
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-26 09:54:40 -0600

    Readding selectDb

--- src/queries-Robert/dataBase/selectDb.js
+++ src/queries-Robert/dataBase/selectDb.js
@@ -5,6 +5,9 @@ let getDataForSelect = (queryInfo, user) => {
     // get db ref from user object
     let db = user.data_baby; // TODO do this a better way
 
+    // user.db_object to get active db for call to ref
+    // user try catch on ref to make sure there are no issues
+
     if (wheres === null || wheres[0] !== '=') {
 
     }
