81c041ca7ba301faac4af5c7de06880d788e8d16
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-28 15:36:39 -0600

    Updated call to database

--- src/queries-Robert/dataBase/selectDb.js
+++ src/queries-Robert/dataBase/selectDb.js
@@ -1,9 +1,14 @@
+/**
+ *
+ * @param queryInfo
+ * @param user: {User}
+ */
 let getDataForSelect = (queryInfo, user) => {
     let wheres = queryInfo.wheres;
     let selectFields = queryInfo.selectFields;
 
     // get db ref from user object
-    let db = user.data_baby; // TODO do this a better way
+    let db = user.db_object;
 
     // user.db_object to get active db for call to ref
     // user try catch on ref to make sure there are no issues
