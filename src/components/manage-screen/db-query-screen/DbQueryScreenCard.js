135b0dae610e5562de98125ef89abb086c14fd2e
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-31 20:06:20 -0600

    Resolved merge confilcts

--- src/components/manage-screen/db-query-screen/DbQueryScreenCard.js
+++ src/components/manage-screen/db-query-screen/DbQueryScreenCard.js
@@ -2,14 +2,10 @@ import React from 'react'
 import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from '@material-ui/core/styles'
-<<<<<<< HEAD:src/components/manage-screen/db-screen/DbScreenCard.js
 import executeQuery from '../../../db-queries/execute';
 import {UserState} from '../../../context/userContext';
-=======
-import {UserState} from "../../../context/userContext";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
->>>>>>> b2f908a2777ced47256ae16f6c5d1844d37f784f:src/components/manage-screen/db-query-screen/DbQueryScreenCard.js
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -54,33 +50,6 @@ const useStyles = makeStyles((theme) => ({
 //need to put it in another div or pass down props to label etc. 
 export default function DbQueryScreenCard() {
     const classes = useStyles();
-<<<<<<< HEAD:src/components/manage-screen/db-screen/DbScreenCard.js
-    const {user} = UserState();
-
-    async function sql_query(query) {
-
-        const result = await executeQuery(query, user);
-        console.log(result);
-    }
-
-    return(
-        <div className={classes.root}>
-            <Card className={classes.card}>
-                <TextField
-                    id="standard-textarea"
-                    label="Query"
-                    multiline
-                    defaultValue="Default Value"
-                    className={classes.textField}
-                    onKeyPress={ (press) => {
-                        if (press.key === 'Enter') {
-                            console.log(press.target.value);
-                            sql_query(press.target.value);
-                        }
-                    }}
-                />
-                <DbObjectDisplay/>
-=======
     const { user } = UserState();
     
     return(
@@ -113,7 +82,6 @@ export default function DbQueryScreenCard() {
                     </div>
                 )}
 
->>>>>>> b2f908a2777ced47256ae16f6c5d1844d37f784f:src/components/manage-screen/db-query-screen/DbQueryScreenCard.js
             </Card>
         </div>
     )
