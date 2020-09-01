f6109bae5546cbbb44b08a17a3ee069d310d03ed
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-08-31 20:08:09 -0600

    Wrote function queryEntireRealTimeCollection

--- src/queries-Robert/dataBase/selectDb.js
+++ src/queries-Robert/dataBase/selectDb.js
@@ -7,14 +7,12 @@ let getDataForSelect = (queryInfo, user) => {
     let wheres = queryInfo.wheres;
     let selectFields = queryInfo.selectFields;
 
-    // get db ref from user object
-    let db = user.db_object;
-
-    // user.db_object to get active db for call to ref
-    // user try catch on ref to make sure there are no issues
-
     if (wheres === null || wheres[0] !== '=') {
-
+        return queryEntireRealTimeCollection(queryInfo, user);
+        // queryEntireRealTimeCollection
+    }
+    else {
+        // executeFilteredRealTimeQuery
     }
 
 }
@@ -23,5 +21,28 @@ let queryEntireRealTimeCollection = (queryInfo, user) => {
     let collection = queryInfo.collection;
     let selectFields = queryInfo.selectFields;
 
-    //const ref =
+    // get db from user object
+    let db = user.db_object;
+
+    if (db === undefined) {
+        throw new Error("queryEntireRealTimeCollection(): database is undefined");
+    }
+
+    const ref = db.ref(collection);
+
+    // get data using once so listener automatically detaches
+    ref.once("value")
+        .then((snapshot) => {
+            // get language specific object rep with .val()
+            let payload = snapshot.val();
+            if (selectFields) {
+                // payload = removeNonSelectedFieldsFromResults(payload, queryInfo);
+            }
+            return payload;
+    });
 }
+
+module.exports = {
+    getDataForSelect: getDataForSelect
+}
+
