9034b0299d43a5d62f881cad06185b6bcbe129b7
jacksonschuler <jacksonschuler11@gmail.com>
2020-09-11 14:06:50 -0600

    more indepth testing done for executeUpdate()

--- src/queries-Robert/queryTests/updateTests.js
+++ src/queries-Robert/queryTests/updateTests.js
@@ -13,24 +13,68 @@ const app = admin.initializeApp({
 const db = app.database();
 
 // Dummy data loaded into database
-const data = {
-    Events: ['cards', 'coding', 'skiing'],
-    Players: ['Robert', 'Ben', 'Jackson'],
-    Scores: {
-        cards: { Ben: 3, Jackson: 4, Robert: 3 },
-        coding: { Ben: 900, Jackson: 1200, Robert: 1500 },
-        skiing: { Ben: 100, Jackson: 20, Robert: 15 }
-    },
-    Winners: { cards: 'Jackson', coding: 'Robert', skiing: 'Ben' }
+const data = {games : {
+        Employees: [
+            {Name: 'Jackson', Number: 15},
+            {Name: 'Robert', Number: 20},
+            {Name: 'Ben', Number: 15},
+        ],
+        Events: ['cards', 'coding', 'skiing'],
+        Players: ['Robert', 'Ben', 'Jackson'],
+        Scores: {
+            cards: { Ben: 3, Jackson: 4, Robert: 3 },
+            coding: { Ben: 900, Jackson: 1200, Robert: 1500 },
+            skiing: { Ben: 100, Jackson: 20, Robert: 15 }
+        },
+        Winners: { cards: 'Jackson', coding: 'Robert', skiing: 'Ben' }
+    }
 };
 
 const execUpdateTests = async () => {
-    // let query = 'update games/Scores/skiing set Jackson=6666, Robert=333';
-    // await fbsql.executeQuery(query, db, true);
 
-    let query = 'update games/Scores/skiing set Jackson=20, Robert=15';
-    let result = await fbsql.executeQuery(query, db, true);
-    console.log(result)
+    // Testing without where statements
+
+    let query0 = 'update games/Scores/skiing set Jackson=200, Robert=150'; //update Jackson to 200, and Robert to 150
+    let expected0 = {games:{
+            Employees: [
+                {Name: 'Jackson', Number: 15},
+                {Name: 'Robert', Number: 20},
+                {Name: 'Ben', Number: 15},
+            ],
+            Events: ['cards', 'coding', 'skiing'],
+            Players: ['Robert', 'Ben', 'Jackson'],
+            Scores: {
+                cards: { Ben: 3, Jackson: 4, Robert: 3 },
+                coding: { Ben: 900, Jackson: 1200, Robert: 1500 },
+                skiing: { Ben: 100, Jackson: 200, Robert: 150 }
+            },
+            Winners: { cards: 'Jackson', coding: 'Robert', skiing: 'Ben' }
+        }
+    };
+    let result0 = await fbsql.executeQuery(query0, db, true);
+    assert.deepStrictEqual(result0, expected0);
+
+    let query1 = 'update games/Players set 2=Timmy'; //update player at index 2 to Timmy
+    let expected1 = {games:{
+            Employees: [
+                {Name: 'Jackson', Number: 15},
+                {Name: 'Robert', Number: 20},
+                {Name: 'Ben', Number: 15},
+            ],
+            Events: ['cards', 'coding', 'skiing'],
+            Players: ['Robert', 'Ben', 'Timmy'],
+            Scores: {
+                cards: { Ben: 3, Jackson: 4, Robert: 3 },
+                coding: { Ben: 900, Jackson: 1200, Robert: 1500 },
+                skiing: { Ben: 100, Jackson: 200, Robert: 150 }
+            },
+            Winners: { cards: 'Jackson', coding: 'Robert', skiing: 'Ben' }
+        }
+    };
+    let result1 = await fbsql.executeQuery(query1, db, true);
+    assert.deepStrictEqual(result1, expected1);
+
+    console.log("*****UPDATE TESTS COMPLETE*****")
 };
 
 execUpdateTests();
