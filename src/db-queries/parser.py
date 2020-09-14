d60c8ae6936498834a9e339525e01ed4ecc8ad25
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-09-14 10:08:10 -0600

    Removing fbsql files from repository

--- src/db-queries/parser.py
+++
@@ -1,9 +0,0 @@
-import sys as sys
-from moz_sql_parser import parse
-import json
-
-try:
-    result = json.dumps(parse(sys.argv[1]))
-    print(result, file=sys.stdout)
-except:
-    print("SQL parser error: invalid query", file=sys.stderr)
