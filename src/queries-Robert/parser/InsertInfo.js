5f299e66d372b43cffb6815e59b0b71dee28adad
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-09-23 08:46:33 -0600

    Added object to hold info for INSERT query

---
+++ src/queries-Robert/parser/InsertInfo.js
@@ -0,0 +1,48 @@
+const QueryInfo = require("QueryInfo").QueryInfo;
+
+class InsertInfo extends QueryInfo {
+
+    /**
+     * Construct a new InsertInfo object.
+     * @param keys: {Array} - array of keys to be added to the database
+     * @param values: {Array} - array of values to be added to the database
+     * @param numInserts {Number} - number of times the given keys and values should
+     *                              be inserted into the database
+     */
+    constructor(keys=null, values=null, numInserts=null) {
+        super();
+        this._keys = keys;
+        this._values = values;
+        this._numInserts = numInserts;
+    }
+
+    /* GETTER METHODS */
+    get keys() {
+        return this._keys;
+    }
+
+    get values() {
+        return this._values;
+    }
+
+    get numInserts() {
+        return this._numInserts;
+    }
+
+    /* SETTER METHODS */
+    set keys(value) {
+        this._keys = value;
+    }
+
+    set values(value) {
+        this._values = value;
+    }
+
+    set numInserts(value) {
+        this._numInserts = value;
+    }
+}
+
+module.exports = {
+    InsertInfo: InsertInfo
+}
