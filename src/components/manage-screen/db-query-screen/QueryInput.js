771d3b4f5b0779fdaa910ea98656744b825c7775
BenBaker <bakerbenjamyn@gmail.com>
2020-09-30 10:46:09 -0600

    got the autocomplete framework and functionality working with static data list

--- src/components/manage-screen/db-query-screen/QueryInput.js
+++ src/components/manage-screen/db-query-screen/QueryInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, makeStyles, TextField} from '@material-ui/core/';
+import {Button, makeStyles, TextField, Paper, List, ListItem, ListItemText} from '@material-ui/core/';
 
 const useStyles = makeStyles((theme) => ({
   textField: {
@@ -14,15 +14,30 @@ const useStyles = makeStyles((theme) => ({
   successfulQuery: {
     borderColor: '#4BB543',
     borderWidth: '3px'
+  },
+  autocompleteList: {
+    width: '90%',
+    marginLeft: '5%',
+    marginRight: '5%',
+    marginTop: '0px'
   }
 }));
 
 export default function QueryInput({input, setInput, setQuery, query}) {
   const classes = useStyles();
   const parser = require('../../../queries-Robert/parser/queryParser'); 
-  
+  /* Triggers the display of the Autocomplete list */
+  const [displayAutocomplete, setDisplayAutocomplete] = React.useState(false);
+  /* Handles the index of the items selected in the autocomplete list */
+  const [autocompleteIndex, setAutocompleteIndex] = React.useState(0);
+
   /* Function used to update the display of the text field */
   const handleInput = (event) => {
+    if(event.target.value[event.target.value.length - 1] === '@'){
+      setDisplayAutocomplete(true)
+    } else {
+      setDisplayAutocomplete(false)
+    }
     setInput(event.target.value)
     setQuery({
         queryString: undefined,
@@ -38,7 +53,28 @@ export default function QueryInput({input, setInput, setQuery, query}) {
       querySuccess: false
     }))
   }
+  
+  /* Function used to handle key presses, which allows of key control of autocomplete list  */
+  const handleKeyPressed = (event) => {
+    if(!displayAutocomplete){return}
+    event.preventDefault();
+    if(event.key === 'ArrowDown') {
+        setAutocompleteIndex((autocompleteIndex + 1) % items.length)
+    }else if(event.key === 'ArrowUp') {
+      setAutocompleteIndex((autocompleteIndex - 1) % items.length)
+    }else if(event.key === 'Enter') {
+      handleAutoComplete(items[autocompleteIndex])
+    }
+  }
 
+  /* Function used to bring in the selected autocomplete item into the textfield*/
+  const handleAutoComplete = (item) => {
+    setInput(input.substring(0, input.length - 1) + item);
+    setDisplayAutocomplete(false)
+    setAutocompleteIndex(0)
+  }
+
+  var items = ['Item1', 'Item2', 'Item3']
   return (
     <>
       <TextField
@@ -60,11 +96,23 @@ export default function QueryInput({input, setInput, setQuery, query}) {
         id="standard-textarea"
         label="Query"
         multiline
-        variant="outlined"    
+        variant="outlined"
         onChange={handleInput}
+        onKeyDown={(event) => handleKeyPressed(event)}
         value={input}
         className={classes.textField}
       />
+      <Paper className={classes.autocompleteList} style={{display: displayAutocomplete ? 'block' : 'none'}} >
+        <List >
+          {items.map(function(item){
+            return (
+              <ListItem selected={items[autocompleteIndex] === item ? true: false} button key={item} onClick={() => handleAutoComplete(item.title)} >
+              <ListItemText primary={item} />
+              </ListItem>
+            )
+          })}
+        </List>
+      </Paper>
     </>
   );
 }
